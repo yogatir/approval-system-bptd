6390cf77abaac725f54cf508f84c7d5f9f876506
yogatir <yogatir.eu@gmail.com>
2024-11-20 02:54:49 +0800

    Remove footer

--- resources/views/add-approval.blade.php
+++ resources/views/add-approval.blade.php
@@ -194,10 +194,6 @@
         </div>
     </div>
 
-    <footer class="bottom-0 w-full absolute bg-gray-800 text-white text-center py-4">
-        <p>&copy; 2024 PNBP BPTD Kelas II Bali</p>
-    </footer>
-
     <script>
         function openConfirmationModal() {
             document.getElementById('confirmationModal').classList.remove('hidden');
